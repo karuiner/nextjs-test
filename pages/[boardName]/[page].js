0a3f7882bb8ad3728b1cc7068036ae5ead597262
karuiner <intaekgong@gmail.com>
2022-12-27 23:27:37 +0900

    미로그인시 로그인으로 전달

--- pages/[boardName]/[page].js
+++ pages/[boardName]/[page].js
@@ -4,10 +4,16 @@ import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import BaseLayout from "../../component/layout";
 import { rootState } from "../../extra/state";
-
 export default function List() {
   const router = useRouter();
   const { boardName, page } = router.query;
+  const [data, setdata] = useRecoilState(rootState);
+
+  useEffect(() => {
+    if (!data.login) {
+      router.push("/login");
+    }
+  });
   return (
     <BaseLayout>
       <div>{[boardName, page]} </div>
